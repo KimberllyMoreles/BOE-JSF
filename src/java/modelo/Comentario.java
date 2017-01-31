a563a9abcb27a64233593aad6ef3040eb99f17ab
KimberllyMoreles <kim.moreles@hotmail.com>
2017-01-31 21:38:18 -0200

    Criação banco

---
+++ src/java/modelo/Comentario.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author aluno
+ */
+@Entity
+@Table(name = "comentario")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Comentario.findAll", query = "SELECT c FROM Comentario c"),
+    @NamedQuery(name = "Comentario.findByIdComentario", query = "SELECT c FROM Comentario c WHERE c.idComentario = :idComentario"),
+    @NamedQuery(name = "Comentario.findByComentario", query = "SELECT c FROM Comentario c WHERE c.comentario = :comentario"),
+    @NamedQuery(name = "Comentario.findByDataCadastro", query = "SELECT c FROM Comentario c WHERE c.dataCadastro = :dataCadastro"),
+    @NamedQuery(name = "Comentario.findByNomeAluno", query = "SELECT c FROM Comentario c WHERE c.nomeAluno = :nomeAluno")})
+public class Comentario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_comentario")
+    private Integer idComentario;
+    @Basic(optional = false)
+    @Column(name = "comentario")
+    private String comentario;
+    @Basic(optional = false)
+    @Column(name = "data_cadastro")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCadastro;
+    @Basic(optional = false)
+    @Column(name = "nome_aluno")
+    private String nomeAluno;
+    @JoinColumn(name = "id_ocorrencia", referencedColumnName = "id_ocorrencia")
+    @ManyToOne(optional = false)
+    private Ocorrencia idOcorrencia;
+
+    public Comentario() {
+    }
+
+    public Comentario(Integer idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public Comentario(Integer idComentario, String comentario, Date dataCadastro, String nomeAluno) {
+        this.idComentario = idComentario;
+        this.comentario = comentario;
+        this.dataCadastro = dataCadastro;
+        this.nomeAluno = nomeAluno;
+    }
+
+    public Integer getIdComentario() {
+        return idComentario;
+    }
+
+    public void setIdComentario(Integer idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public Ocorrencia getIdOcorrencia() {
+        return idOcorrencia;
+    }
+
+    public void setIdOcorrencia(Ocorrencia idOcorrencia) {
+        this.idOcorrencia = idOcorrencia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idComentario != null ? idComentario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Comentario)) {
+            return false;
+        }
+        Comentario other = (Comentario) object;
+        if ((this.idComentario == null && other.idComentario != null) || (this.idComentario != null && !this.idComentario.equals(other.idComentario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Comentario[ idComentario=" + idComentario + " ]";
+    }
+    
+}
---
+++ src/java/modelo/Ocorrencia.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author aluno
+ */
+@Entity
+@Table(name = "ocorrencia")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Ocorrencia.findAll", query = "SELECT o FROM Ocorrencia o"),
+    @NamedQuery(name = "Ocorrencia.findByIdOcorrencia", query = "SELECT o FROM Ocorrencia o WHERE o.idOcorrencia = :idOcorrencia"),
+    @NamedQuery(name = "Ocorrencia.findByDataCadastro", query = "SELECT o FROM Ocorrencia o WHERE o.dataCadastro = :dataCadastro"),
+    @NamedQuery(name = "Ocorrencia.findByNomeSolicitante", query = "SELECT o FROM Ocorrencia o WHERE o.nomeSolicitante = :nomeSolicitante"),
+    @NamedQuery(name = "Ocorrencia.findByTipoOcorrencia", query = "SELECT o FROM Ocorrencia o WHERE o.tipoOcorrencia = :tipoOcorrencia")})
+public class Ocorrencia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_ocorrencia")
+    private Integer idOcorrencia;
+    @Basic(optional = false)
+    @Column(name = "data_cadastro")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCadastro;
+    @Column(name = "nome_solicitante")
+    private String nomeSolicitante;
+    @Basic(optional = false)
+    @Column(name = "tipo_ocorrencia")
+    private String tipoOcorrencia;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idOcorrencia")
+    private Collection<Comentario> comentarioCollection;
+
+    public Ocorrencia() {
+    }
+
+    public Ocorrencia(Integer idOcorrencia) {
+        this.idOcorrencia = idOcorrencia;
+    }
+
+    public Ocorrencia(Integer idOcorrencia, Date dataCadastro, String tipoOcorrencia) {
+        this.idOcorrencia = idOcorrencia;
+        this.dataCadastro = dataCadastro;
+        this.tipoOcorrencia = tipoOcorrencia;
+    }
+
+    public Integer getIdOcorrencia() {
+        return idOcorrencia;
+    }
+
+    public void setIdOcorrencia(Integer idOcorrencia) {
+        this.idOcorrencia = idOcorrencia;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getNomeSolicitante() {
+        return nomeSolicitante;
+    }
+
+    public void setNomeSolicitante(String nomeSolicitante) {
+        this.nomeSolicitante = nomeSolicitante;
+    }
+
+    public String getTipoOcorrencia() {
+        return tipoOcorrencia;
+    }
+
+    public void setTipoOcorrencia(String tipoOcorrencia) {
+        this.tipoOcorrencia = tipoOcorrencia;
+    }
+
+    @XmlTransient
+    public Collection<Comentario> getComentarioCollection() {
+        return comentarioCollection;
+    }
+
+    public void setComentarioCollection(Collection<Comentario> comentarioCollection) {
+        this.comentarioCollection = comentarioCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idOcorrencia != null ? idOcorrencia.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ocorrencia)) {
+            return false;
+        }
+        Ocorrencia other = (Ocorrencia) object;
+        if ((this.idOcorrencia == null && other.idOcorrencia != null) || (this.idOcorrencia != null && !this.idOcorrencia.equals(other.idOcorrencia))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Ocorrencia[ idOcorrencia=" + idOcorrencia + " ]";
+    }
+    
+}
